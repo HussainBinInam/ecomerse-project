a5f587007fa6c01d2f4a8fd075e9e6a512f70356
Colorlessowl <hbinam25@gmail.com>
2021-12-31 21:30:45 +0500

    I added the header and the stylings for it

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 import {Route, Routes} from 'react-router-dom';
 import HomePage from './Page/homepage/Homepage.component';
 import ShopPage  from './Page/shop/shop.component';
-
+import Header from './Componenet/header/header.componrnt';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Routes>
         <Route exact path= '/' element= {<HomePage/>}  /> {/*  this raouts to the the MainPage basically the homepage and its path is defined by the path parameter pased into the raout */}
         <Route path= '/Shop' element= {<ShopPage/>}  /> {/* this raouts to the the page called A genre and its path is defined by the path parameter pased into the raout*/}
